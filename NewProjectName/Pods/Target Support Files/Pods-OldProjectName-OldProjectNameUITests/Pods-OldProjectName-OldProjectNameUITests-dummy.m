6a35d113cec76082b083b037205779e5c1912e64
Darren <darren@MacBook-Pro.lan>
2020-08-12 19:01:39 +0200

    remove all

--- NewProjectName/Pods/Target Support Files/Pods-OldProjectName-OldProjectNameUITests/Pods-OldProjectName-OldProjectNameUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OldProjectName_OldProjectNameUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OldProjectName_OldProjectNameUITests
-@end
--- NewProjectName/Pods/Target Support Files/Pods-OldProjectName/Pods-OldProjectName-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OldProjectName : NSObject
-@end
-@implementation PodsDummy_Pods_OldProjectName
-@end

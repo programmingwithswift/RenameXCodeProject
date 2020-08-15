f327b861a51fec9e1fc3a7aa1dcbd1f749fa771b
Darren <darren@MacBook-Pro.lan>
2020-08-15 12:58:59 +0200

    renaming

---
+++ NewProjectName/Pods/Target Support Files/Pods-NewProjectName-NewProjectNameUITests/Pods-NewProjectName-NewProjectNameUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewProjectName_NewProjectNameUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NewProjectName_NewProjectNameUITests
+@end
